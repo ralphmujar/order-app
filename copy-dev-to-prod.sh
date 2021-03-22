ccfb22cb56cc45bb44851b00d6d319640d56915d
ralph william mujar <ralphwilliammujar@ralphs-MacBook-Air.local>
2021-03-22 14:08:27 +0800

    remove unecessary files

--- copy-dev-to-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-shopt -s extglob
-echo "-- Copying dev to production --"
-cp -R !(mysqldata) ../mysterious-ocean-67929/ && cd ../mysterious-ocean-67929/ && ./apply-prod-config.sh
-shopt -u extglob
